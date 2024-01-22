c3b456069be75d0c15985afa234add22052acee7
Candicezile <zilecandice@gmail.com>
2024-01-22 15:13:41 +0200

    Initial commit

--- Templates/my-react-app/src/pages/Dashboard.js
+++ Templates/my-react-app/src/pages/Dashboard.js
@@ -2,32 +2,50 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Dashboard = ({ children }) => {
-    return (
-        <div>
-            <h1>Dashboard</h1>
+  const getLocalStorageLength = () => {
+    const storedData = localStorage.getItem('bookList');
+    const storedData1 = localStorage.getItem('nameList');
 
-            <header>
-                {children}
-            </header>
-             
-             <main>
-                <nav>
-                    <ul>
-          <button className='button5'>
-          <Link to="/home">Home</Link><br></br>
+    const bookList = JSON.parse(storedData) || [];
+    const nameList = JSON.parse(storedData1) || [];
 
-          </button>
+    const length = bookList.length;
+    const length1 = nameList.length;
 
+    return { length, length1 };
+  };
 
-                    
-                    </ul>
-                </nav>
-             </main>
-    
+  const { length: localStorageLength, length1: localStorageLength1 } = getLocalStorageLength();
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+      <div>
+          <p>Book's length: {localStorageLength}</p>
+          <p>Name's length: {localStorageLength1}</p>
         </div>
-    );
+
+      <header>
+        {children}
+      </header>
+
+      <main>
+
+
+        <nav>
+          <ul>
+            <button className='button5'>
+              <Link to="/home">Home</Link><br></br>
+            </button>
+          </ul>
+        </nav>
+
+        
+      </main>
+    </div>
+  );
 };
 
-
-
 export default Dashboard;
+
